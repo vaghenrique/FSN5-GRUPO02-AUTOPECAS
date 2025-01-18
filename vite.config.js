3d9e2c738fe28f65b9d2ac3e97b9d30e17238ebb
OctoGab <138169563+GabCostta@users.noreply.github.com>
2025-01-18 22:47:46 +0000

    git commit -m "Corrige base para GitHub Pages"

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/FSN5-GRUPO02-AUTOPECAS/',
   plugins: [react()],
   resolve: {
     alias: {
